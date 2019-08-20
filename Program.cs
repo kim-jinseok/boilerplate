0fb24cceb376bc159d7c7737bfc18b98b3861725
Sue712 <seunghee.sh@gmail.com>
2019-08-20 11:00:50 +0900

    이전 상태로 복구

--- Program.cs
+++ Program.cs
@@ -21,11 +21,12 @@ namespace jlsCore
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-              .UseKestrel()
-              .UseContentRoot(Directory.GetCurrentDirectory())
-              .UseIISIntegration()
-              .UseStartup<Startup>()
-              .UseUrls("http://localhost:5001");
+            .UseStartup<Startup>();
+        //   .UseKestrel()
+        //   .UseContentRoot(Directory.GetCurrentDirectory())
+        //   .UseIISIntegration()
+        //   .UseStartup<Startup>()
+        //   .UseUrls("http://localhost:5001");
     }
 }
 
